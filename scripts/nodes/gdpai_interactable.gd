d00a89407f0b0dfe081d2c0f5602c6fb30f42ce2
CodeCorrupt <tylerhoyt93@gmail.com>
2025-09-27 16:55:52 -0400

    Update SpatialAction to track the target object if it moves
    Adds new attribute to GdPAIInteractable to define the max distance the
    target is allowed to drift from it's planning-time location before the
    action is considered to have failed.
    
    The `max_interaction_distance` was also used to trigger the navigation
    target update. This was chosen since it's the max distance an agent
    can interact, so theoretically, if the target moves this distance,
    we'll no longer be able to reach it if we were aiming for the center.

--- scripts/nodes/gdpai_interactable.gd
+++ scripts/nodes/gdpai_interactable.gd
@@ -5,6 +5,11 @@ extends GdPAIObjectData
 ## An object can specify how far away it need be.  Values <=0 disable the check.
 @export var max_interaction_distance: float = 2
 
+## The maximum distance an object can move from its planning-time position before
+## an associated spatial action is failed.
+## Values less than 0 (n<0) never trigger failure.
+@export var max_drift_from_plan: float = -1
+
 
 # Override
 func get_group_labels():
@@ -16,4 +21,5 @@ func copy_for_simulation() -> GdPAIObjectData:
 	var new_data: GdPAIInteractable = GdPAIInteractable.new()
 	assign_uid_and_entity(new_data)
 	new_data.max_interaction_distance = max_interaction_distance
+	new_data.max_drift_from_plan = max_drift_from_plan
 	return new_data
