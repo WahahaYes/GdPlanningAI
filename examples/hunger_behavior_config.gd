198d5469b4f820588fce59de1184b313c1fb3308
WahahaYes <ethanrwilson1998@gmail.com>
2025-12-23 16:41:39 -0500

    move hunger behavior config to class, where it flows to the HungerPropertyUpdater

--- examples/hunger_behavior_config.gd
+++ examples/hunger_behavior_config.gd
@@ -2,11 +2,14 @@ class_name HungerBehaviorConfig
 extends GdPAIBehaviorConfig
 ## Behavior configuration for agents that experience hunger.
 
+## How much hunger drops per second.
+@export var hunger_decay: float = 5.0
+
 
 ## Override to automatically set up hunger-related configuration.
 func _init():
 	goals.append(SampleHungerGoal.new())
 	goals.append(SampleWanderGoal.new())
 	self_actions.append(SampleWanderAction.new(256))
-	property_updaters.append(HungerPropertyUpdater.new())
+	property_updaters.append(HungerPropertyUpdater.new(hunger_decay))
 	initial_properties = { "hunger": 100.0 }
