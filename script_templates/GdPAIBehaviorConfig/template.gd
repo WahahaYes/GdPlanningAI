a83efc8ee9d6ae99e5ea6785fef3b888f887733a
WahahaYes <ethanrwilson1998@gmail.com>
2025-12-25 12:23:07 -0500

    create default templates for BehaviorConfig and PropertyUpdater

---
+++ script_templates/GdPAIBehaviorConfig/template.gd
@@ -0,0 +1,15 @@
+# meta-description: GdPAI BehaviorConfig template.
+# meta-default: true
+extends GdPAIBehaviorConfig
+
+# Add your configurable properties here with @export for serialization.
+@export var example_parameter: float = 1.0
+
+
+func _init() -> void:
+	# Configure goals, self_actions, and property_updaters here.
+
+	# You can pass @export properties to your custom classes.
+	goals.append(Goal.new())
+	self_actions.append(Action.new())
+	property_updaters.append(TemplatePropertyUpdater.new(example_parameter))
