e96b2e19e5183fbf4fdb86a84002e2878b492918
WahahaYes <ethanrwilson1998@gmail.com>
2025-12-25 11:44:49 -0500

    create wander behavior and a base agent config

---
+++ examples/wander/wander_behavior_config.gd
@@ -0,0 +1,11 @@
+class_name WanderBehaviorConfig
+extends GdPAIBehaviorConfig
+## Behavior configuration for agents that wander around the environment.
+
+## Distance the agent will wander in each action.
+@export var wander_distance: float = 20.0
+
+
+func _init():
+	goals.append(SampleWanderGoal.new())
+	self_actions.append(SampleWanderAction.new(wander_distance))
