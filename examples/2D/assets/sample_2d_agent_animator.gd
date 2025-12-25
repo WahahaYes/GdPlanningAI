d9dcce2f0ef56814f8d6f471b07004bfbff0fc11
Gawain <info@konspirationen.nu>
2025-12-25 14:52:25 +0100

    Refactor SampleAgent debug label
    moves debug label out of 2d animator node, to make it clearer what's
    part of the GdPAI setup and waht's not. Also gives a clear location
    of where the debug label exists if one wants to inspect it.

--- examples/2D/assets/sample_2d_agent_animator.gd
+++ examples/2D/assets/sample_2d_agent_animator.gd
@@ -7,10 +7,6 @@ extends Node
 @export var animated_sprite: AnimatedSprite2D
 ## Velocity threshold for sprite animation.
 @export var idle_threshold: float = 1
-## Reference to the GdPAI agent.
-@export var gdpai_agent: GdPAIAgent
-## Reference to a textual display which shows some attributes of the agent.
-@export var display_text: Label
 
 
 func _process(_delta: float) -> void:
@@ -24,25 +20,3 @@ func _process(_delta: float) -> void:
 		animated_sprite.scale.x = -1
 	elif entity.linear_velocity.x > 0:
 		animated_sprite.scale.x = 1
-
-	# Hacky way to display the goal and action being considered at this moment.
-	var hunger: float = gdpai_agent.blackboard.get_property("hunger")
-	var goal_text: String
-	if gdpai_agent.get_current_goal() != null:
-		goal_text = gdpai_agent.get_current_goal().get_title()
-
-	var action_text: String
-	if (
-		gdpai_agent.get_current_plan() != null
-		and gdpai_agent.get_current_plan().get_plan().size() > 0
-	):
-		var step: int = min(
-			gdpai_agent.get_current_plan_step(),
-			gdpai_agent.get_current_plan().get_plan().size() - 1,
-		)
-		var action: Action = gdpai_agent.get_current_plan().get_plan()[step]
-		action_text = action.get_title()
-
-	display_text.text = (
-		"Hunger: %.f\nGoal: %s\nCurrent\nAction: %s" % [hunger, goal_text, action_text]
-	)
