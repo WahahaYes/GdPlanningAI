c72640e70970a61d50565bed38c988ef2d0a6ab4
WahahaYes <ethanrwilson1998@gmail.com>
2026-07-04 13:34:25 -0400

    clarify goal reward freq in template

--- script_templates/Goal/template.gd
+++ script_templates/Goal/template.gd
@@ -3,6 +3,8 @@
 extends Goal
 
 
+## Called every planning cycle to re-prioritize this goal.
+## Return value can change frame-to-frame to reflect dynamic conditions (e.g. rising hunger).
 # Override
 func compute_reward(_agent: GdPAIAgent) -> float:
 	return 0
